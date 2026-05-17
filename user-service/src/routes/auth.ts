1b714eecbded121da62e00fbc6a2b7f485d1e5ae
niamul007 <am3994566@gmail.com>
2026-05-17 16:15:07 +0600

    adding new routes

--- user-service/src/routes/auth.ts
+++ user-service/src/routes/auth.ts
@@ -1,9 +1,10 @@
-import { register,login } from "../controllers/authController";
+import { register,login , verify } from "../controllers/authController";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/verify", verify);
 
 export default router;
