2fc27338929d9db348a14acab653fd7ad886f5fd
niamul007 <am3994566@gmail.com>
2026-05-17 16:14:43 +0600

    adding verify fn to check the user

--- user-service/src/controllers/authController.ts
+++ user-service/src/controllers/authController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import prisma from "../config/prisma";
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
-  const { name ,email, password } = req?.body;
+  const { name, email, password } = req?.body;
   const user = await prisma.user.findUnique({ where: { email } });
   if (user) {
     return res.status(400).json({ message: "User already exist" });
@@ -25,13 +25,28 @@ export const login = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "User not exist" });
   }
   const isValidPass = await bcrypt.compare(password, user?.password);
-  if(!isValidPass){
-    return res.status(400).json({message: "Invalid credentials"})
+  if (!isValidPass) {
+    return res.status(400).json({ message: "Invalid credentials" });
   }
   const token = jwt.sign(
-    {email},
+    { id: user.id, email: user.email },
     process?.env?.JWT_SECRET || "SECRET",
-    {expiresIn: "1d"}
-  )
-  return res.status(200).json({token})
+    { expiresIn: "1d" },
+  );
+  return res.status(200).json({ token });
+};
+
+export const verify = async (req: Request, res: Response) => {
+  const auth = req?.headers?.authorization;
+  if (!auth) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = auth.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process?.env?.JWT_SECRET || "SECRET");
+    return res.status(200).json({ message: "Authorized", user: decoded });
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 };
